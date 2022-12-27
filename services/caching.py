1a4e26679fdaa928243c3a7f3b35721cc5c2e1ca
Sergey Ivanov <freezy5g@gmail.com>
2022-12-28 00:24:30 +0700

    add caching service based on redis client

---
+++ services/caching.py
@@ -0,0 +1,21 @@
+import json
+from typing import Optional, Union
+
+from clients.redis import redis_client
+
+
+class CachingService:
+    @staticmethod
+    async def get_value(key: str) -> Optional[Union[str, dict, list, bytes, int, float]]:
+        value = await redis_client.get(key)
+        if value:
+            return json.loads(value)
+
+    @staticmethod
+    async def set_value(key: str, value: Union[str, dict, list, bytes, int, float]) -> None:
+        new_value = json.dumps(value)
+        return await redis_client.set(key, new_value)
+
+    @staticmethod
+    async def delete_key(key: str) -> None:
+        return await redis_client.delete(*[key])
