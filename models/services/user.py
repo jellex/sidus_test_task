a414e796023ae02a7be4e2e071828842db9151a4
Sergey Ivanov <freezy5g@gmail.com>
2022-12-27 23:41:50 +0700

    add user db service

---
+++ models/services/user.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from pydantic.types import PositiveInt
+
+from clients.database import get_session
+from models.user import User
+
+
+class UserDBService:
+    @staticmethod
+    def create_user(login: str, password: str) -> User:
+        with get_session() as session:
+            user = User(login=login, password=password)
+            session.add(user)
+            session.commit()
+            session.refresh(user)
+        return user
+
+    @staticmethod
+    def get_user(user_id: Optional[PositiveInt] = None, login: Optional[str] = None) -> Optional[User]:
+        filters = []
+        if user_id:
+            filters.append(User.id == user_id)
+        if login:
+            filters.append(User.login == login)
+        with get_session() as session:
+            result = session.query(User).filter(*filters).first()
+        return result
+
+    @staticmethod
+    def update_user(user_model: User, password: Optional[str] = None, name: Optional[str] = None) -> User:
+        if password:
+            user_model.password = password
+        if name:
+            user_model.name = name
+        with get_session() as session:
+            session.add(user_model)
+        return user_model
