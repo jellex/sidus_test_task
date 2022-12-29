b2d0a96b6aaa9ab2036ef21049ad8fe6622f2736
Sergey Ivanov <freezy5g@gmail.com>
2022-12-29 22:14:53 +0700

    add new error

--- utils/errors.py
+++ utils/errors.py
@@ -1,2 +1,6 @@
 class UserDoesNotExistError(ValueError):
     """Base error for user does not exist error"""
+
+
+class UserAlreadyExistError(ValueError):
+    """Base error for already existed user error"""
