98d3afe18a09836e6e02b2eb8ac88422bb3099ab
Sergey Ivanov <freezy5g@gmail.com>
2023-01-08 23:52:01 +0700

    fix config class

--- config/__init__.py
+++ config/__init__.py
@@ -1,11 +1,11 @@
-from pydantic import AnyHttpUrl
+from pydantic import AnyHttpUrl, BaseSettings
 
 from config.auth import AuthConfig
 from config.database import DatabaseConfig
 from config.redis import RedisConfig
 
 
-class Config(AuthConfig, DatabaseConfig, RedisConfig):
+class Config(BaseSettings, AuthConfig, DatabaseConfig, RedisConfig):
     SERVER_HOST: AnyHttpUrl = "http://localhost:8000"
     WORKERS: int = 1
     IS_DEBUG: bool = True
