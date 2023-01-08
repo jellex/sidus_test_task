2d58c632b0c59c209df24dd502d0ac222f09ff3c
Sergey Ivanov <freezy5g@gmail.com>
2023-01-08 23:40:39 +0700

    reformat config file for database

--- config/database.py
+++ config/database.py
@@ -2,6 +2,4 @@ from pydantic import AnyUrl, BaseModel
 
 
 class DatabaseConfig(BaseModel):
-    DATABASE_URL: AnyUrl = (
-        "postgresql://sidus:Nuwr5bbT@localhost/sidus_backend"
-    )
+    DATABASE_URL: AnyUrl = "postgresql://sidus:Nuwr5bbT@localhost/sidus_backend"
