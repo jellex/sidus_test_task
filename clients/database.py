c7ca600bda83c02e9da2114cce48dcc625dbc158
Sergey Ivanov <freezy5g@gmail.com>
2023-01-08 20:44:28 +0700

    little rewrite get_session contextmanager

--- clients/database.py
+++ clients/database.py
@@ -10,8 +10,10 @@ engine = create_engine(config.DATABASE_URL, pool_size=3, max_overflow=0, connect
 
 
 @contextmanager
-def get_session() -> Session:
-    session = Session(engine)
+def get_session(engine_=None) -> Session:
+    if not engine_:
+        engine_ = engine
+    session = Session(engine_)
     try:
         yield session
     finally:
